1ff021e560788e2ae0e7fa391d9379b5db9be62f
scige <panf06@gmail.com>
2012-09-25 16:11:01 +0800

    modify user interface

--- app/controllers/query_controller.rb
+++ app/controllers/query_controller.rb
@@ -1,15 +1,26 @@
+# coding: utf-8
+
 class QueryController < ApplicationController
     def index
-        @log_data_array = LogData.find_all_by_req_originalUrl(params[:url], :limit => 20)
+        @req_originalUrl = params[:url]
+        @log_data_array = LogData.find_all_by_req_originalUrl(@req_originalUrl, 
+                                                              :limit => 20,
+                                                              :order => "log_time DESC, server_name")
         if @log_data_array.empty?
-            redirect_to "/404.html"
+            flash[:notice] = "没有该url的日志: " + params[:url]
+            flash[:notice] += ", 请等待一段时间或查询其他url"
+            redirect_to :action => "noresult"
         end
     end
 
     def show
         @log_data = LogData.find_by_id(params[:id])
         if nil == @log_data
-            render :text => "no result"
+            flash[:notice] = "参数错误, 没有该url的日志"
+            redirect_to :action => "noresult"
         end
+    end
+
+    def noresult
     end
 end
